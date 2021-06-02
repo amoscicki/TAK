9049150ceb256e2516fd1ef24234352deb5d7727
Arek <arkadiuszmoscicki@gmail.com>
2021-06-02 19:36:41 +0100

    mutant

--- Haskell.java
+++ Haskell.java
@@ -143,13 +143,15 @@ public class Haskell {
                 " " + arg + ")"
 
         };
+        if (s.length() > arg.length()) {
+            if (s.substring(0, arg.length() + 1).equals(arg + " "))
+                s = s.replaceFirst(arg, wartoscArg);
 
-        if (s.substring(0, arg.length() + 1).equals(arg + " "))
-            s = s.replaceFirst(arg, wartoscArg);
-
-        if (s.substring(s.length() - (arg.length() + 1), s.length()).equals(" " + arg))
-            s = s.replaceFirst("(?s)" + arg + "(?!.*?" + arg + ")", wartoscArg);
-
+            if (s.substring(s.length() - (arg.length() + 1), s.length()).equals(" " + arg))
+                s = s.replaceFirst("(?s)" + arg + "(?!.*?" + arg + ")", wartoscArg);
+        } else if (s.equals(arg)) {
+            s = wartoscArg;
+        }
         s = s.replace(patterns[0], " " + wartoscArg + " ");
 
         s = s.replace(patterns[1], "(" + wartoscArg + " ");
@@ -173,19 +175,77 @@ public class Haskell {
         return "{arg" + argNumbers.size() + "}";
     }
 
-    public static String lambdaParsuj(String str, String[] wartosciArg) {
-        String[] listaArgumentow = str.substring(str.indexOf("\\") + 1, str.indexOf("->")).trim().split(" ");
-        str = str.substring(str.indexOf("->") + 2).trim();
+    public static String lambdaParsuj(String polecenie, TreeMap<String, String> args,
+            TreeMap<String, String> lambdaArgs) {
 
-        // zamien nazwe funkcji i nastepujace argumenty na operacje
-        for (int i = 0; i < listaArgumentow.length; i++) {
-            if (wartosciArg[i].equals("{arg}")) {
-                wartosciArg[i] = lambdaNumerujArg(str);
+        System.out.println(
+
+                "<---\nfunkcja \t" + "\n" + "polecenie \t" + polecenie + "\n" + "args \t" + args.keySet() + " "
+                        + args.values() + "\n" + "lambdaArgs \t" + lambdaArgs.keySet() + " " + lambdaArgs.values()
+                        + "\n--->"
+
+        );
+
+        if (indeksyNawiasow(polecenie)[0] == 0 && indeksyNawiasow(polecenie)[1] == polecenie.length() - 1)
+            polecenie = polecenie.substring(1, polecenie.length() - 1);
+
+        if (args.size() > 0) {
+            for (Map.Entry<String, String> arg : args.entrySet()) {
+
+                polecenie = zamienArgumenty(polecenie, arg.getKey(), arg.getValue());
             }
-            str = "[" + zamienArgumenty(str, listaArgumentow[i], wartosciArg[i]) + "]";
         }
 
-        return str;
+        //zanim to trzeba posprawdzac czy funkcja sie zaczyna od tego!
+        polecenie = polecenie.substring(0, polecenie.indexOf("\\"))
+                + polecenie.substring(polecenie.indexOf("->") + 2).trim();
+        if (polecenie.indexOf("\\") > -1) {
+            for (int j = 0; j < lambdaIleArg(polecenie); j++) {
+                lambdaArgs.put(
+                        polecenie.substring(polecenie.indexOf("\\") + 1, polecenie.indexOf("->")).trim().split(" ")[j],
+                        "{arg}");
+            }
+
+            polecenie = lambdaParsuj(polecenie, args, lambdaArgs);
+        }
+
+        System.out.println(polecenie);
+
+        List<String> polList = new ArrayList<String>(
+                Arrays.asList(polecenie.replace("[", "  ").replace("]", "  ").split("  ")));
+
+        for (int l = 0; l < polList.size(); l++) {
+            if (!polList.get(l).equals(""))
+                if (polList.get(l).charAt(0) == ' ')
+                    polList.set(l, polList.get(l).replaceFirst(" ", ":"));
+        }
+        polecenie = polList.stream().collect(Collectors.joining(" "));
+        polList = new ArrayList<String>(Arrays.asList(polecenie.split(":")));
+
+        System.out.println(polList);
+
+        for (int i = polList.size() - 1; i >= 0; i--) {
+            ArrayList<String> argsList = lambdaHoldery(polList.get(i));
+            for (int j = 0; j < argsList.size(); j++) {
+                try {
+                    polList.set(i, polList.get(i).replace(argsList.get(j), polList.get(i + j + 1)));
+                    polList.remove(i + j + 1);
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+            }
+
+        }
+        polecenie = polList.stream().collect(Collectors.joining(" "));
+
+        for (Map.Entry<String, String> larg : lambdaArgs.entrySet()) {
+
+            String largv = (larg.getValue().equals("{arg}")) ? lambdaNumerujArg(polecenie) : larg.getValue();
+            polecenie = zamienArgumenty(polecenie.trim(), larg.getKey(), largv);
+
+        }
+
+        return "[" + polecenie + "]";
     }
 
     public static boolean tylkoLiczbyIInfixy(String s) {
@@ -276,38 +336,13 @@ public class Haskell {
                     + str.substring(indeksyNawiasow[1] + 1), strArr);
         }
 
-        if (hmPolecenia.get(str) != null) {
+        if (hmPolecenia.get(str) != null && !czyLambda(str)) {
             // troche nieeleganckie wylapywanie overloadow ale przy naszych zalozeniach powinno dzialac...
             str = str.replace(str, parsuj(hmPolecenia.get(str), strArr));
         }
 
         String[] slowaPolecenia = slowaPolecenia(str);
-        /*
-        if (lambdaHoldery.size() > 0) {
-            for (int i = 0; i < slowaPolecenia.length; i++) {
-                if (lambdaHoldery(slowaPolecenia[i]).size() > 0 && i + 1 < slowaPolecenia.length) {
-                    slowaPolecenia[i] = zamienArgumenty(slowaPolecenia[i], lambdaHoldery(slowaPolecenia[i]).get(0),
-                            slowaPolecenia[i + 1]);
-                }
-            }
-        }
-        /*
-        if (lambdaHoldery.size() > 0) {
-        
-            for (int j = 0; j < lambdaHoldery.size(); j++)
-                try {
-                    zamienArgumenty(str, lambdaHoldery.get(j), slowaPolecenia[i + j + 1]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                }
-        
-        }
-        */
-        if (str.indexOf("[") != 0) {
-            str = str.replace("[", "(").replace("]", ")");
-            for (int i = 0; i < slowaPolecenia.length; i++) {
-                slowaPolecenia[i] = slowaPolecenia[i].replace("[", "(").replace("]", ")");
-            }
-        }
+
         for (int i = 0; i < slowaPolecenia.length; i++) {
             String s = slowaPolecenia[i].toString();
 
@@ -318,78 +353,162 @@ public class Haskell {
             strArr = addArrays(strArr, newStrArr);
 
             if (czyLambda(s)) { //lambda
-                if (hmPolecenia.get(s) != null) {
-                    String[] wartosciArg = new String[lambdaIleArg(hmPolecenia.get(s))];
-                    String target = s;
-                    for (int j = 0, k = 0; j < wartosciArg.length; j++) {
-                        try {
-                            wartosciArg[j] = slowaPolecenia[i + j + 1];
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            if (strArr.length - 1 < k)
-                                wartosciArg[j] = "{arg}";
-                            else {
-                                wartosciArg[j] = strArr[k];
-                                newStrArr = new String[strArr.length - 1];
-                                System.arraycopy(strArr, k + 1, newStrArr, 0, newStrArr.length);
-                                strArr = newStrArr;
-                                System.out.println(Arrays.toString(strArr));
-                                k++;
-                            }
-                        }
-                        target += " " + wartosciArg[j];
-
-                    }
-                    i += wartosciArg.length - 1;
-                    str = str.replace(target, parsuj(lambdaParsuj(hmPolecenia.get(s), wartosciArg), strArr));
-                } else // tu oprocz lambda abstrakcji mamy rowniez "normalne" argumenty funkcji jesli wystepuja overloady moga nastapic dodatkowe komplikacje
-                {
-
-                    Map<String, String> funkcje = hmPolecenia.entrySet().stream()
+                // nazwa args[] = \ lambdaArgs[] -> polecenie
+                TreeMap<String, String> args = new TreeMap<String, String>();
+                TreeMap<String, String> lambdaArgs = new TreeMap<String, String>();
+                String polecenie = hmPolecenia.get(str);
+                if (polecenie == null)
+                    polecenie = hmPolecenia.get(s);
+                String target = "";
+                if (polecenie == null) {
+                    //sa argumenty funkcji
+                    Map<String, String> pasujaceFunkcje = hmPolecenia.entrySet().stream()
                             .filter(entry -> entry.getKey().startsWith(s))
                             .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
-
-                    for (Map.Entry<String, String> entry : funkcje.entrySet()) {
+                    int k = 0;
+                    for (Map.Entry<String, String> entry : pasujaceFunkcje.entrySet()) {
 
                         String[] deklaracjaFunkcji = entry.getKey().split(" ");
 
                         boolean jestLiczba = true;
                         int iloscArgumentow = deklaracjaFunkcji.length;
-                        String value = entry.getValue();
+                        target = deklaracjaFunkcji[0];
 
-                        String[] wartosciArg = new String[lambdaIleArg(value)];
-
-                        String target = deklaracjaFunkcji[0];
                         for (int j = 1; j < iloscArgumentow; j++) {
                             jestLiczba = jestLiczba(deklaracjaFunkcji[j]);
+                            String wartArg;
                             if (!jestLiczba) {
-                                value = zamienArgumenty(value, deklaracjaFunkcji[j], slowaPolecenia[j]);
-                                target += " " + slowaPolecenia[j];
-                            }
-                        }
 
-                        for (int j = 0, k = 0; j < wartosciArg.length; j++) {
-                            try {
-                                wartosciArg[j] = slowaPolecenia[i + j + iloscArgumentow];
-                            } catch (ArrayIndexOutOfBoundsException e) {
-                                if (/*strArr.length - 1 < k*/true)
-                                    wartosciArg[j] = "{arg}";
-                                else {
-                                    wartosciArg[j] = strArr[k];
-                                    newStrArr = new String[strArr.length - 1];
-                                    System.arraycopy(strArr, k + 1, newStrArr, 0, newStrArr.length);
-                                    strArr = newStrArr;
-                                    System.out.println(Arrays.toString(strArr));
-                                    k++;
+                                try {
+                                    wartArg = slowaPolecenia[i + j];
+                                    target += " " + wartArg;
+                                } catch (ArrayIndexOutOfBoundsException e) {
+                                    wartArg = "{arg}";
                                 }
+                                args.put(deklaracjaFunkcji[j], wartArg);
+                                k++;
+
                             }
+
                         }
-                        i += iloscArgumentow - 1;
                         if (!jestLiczba)
-                            str = str.replace(target, parsuj(lambdaParsuj(value, wartosciArg), strArr));
+                            polecenie = entry.getValue();
+
+                    }
+                    for (int j = 0; j < lambdaIleArg(polecenie); j++) {
+
+                        String wartArg;
+                        try {
+                            wartArg = slowaPolecenia[args.size() + 1 + i + j];
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            wartArg = "{arg}";
+                        }
+                        lambdaArgs.put(polecenie.substring(polecenie.indexOf("\\") + 1, polecenie.indexOf("->")).trim()
+                                .split(" ")[j], wartArg);
+                    }
+                    i += k;
+                } else if (polecenie != null) {
+                    target = s;
+                    //args puste
+                    //tylko lambdaArgs
+                    int k = i;
+                    for (int j = 0; j < lambdaIleArg(polecenie); j++) {
+
+                        String wartArg;
+                        try {
+                            wartArg = slowaPolecenia[1 + k + j];
+                            target += " " + wartArg;
+                            i++;
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            wartArg = "{arg}";
+                        }
+                        lambdaArgs.put(polecenie.substring(polecenie.indexOf("\\") + 1, polecenie.indexOf("->")).trim()
+                                .split(" ")[j], wartArg);
+                    }
+
+                }
+
+                // System.out.println(lambdaParsuj(polecenie, args, lambdaArgs));
+                str = str.replace(target, lambdaParsuj(polecenie, args, lambdaArgs));
+
+                //str.substring(str.indexOf("\\") + 1, str.indexOf("->")).trim().split(" ").length;
+                /*
+                
+                if (hmPolecenia.get(s) != null) {
+                String[] wartosciArg = new String[lambdaIleArg(hmPolecenia.get(s))];
+                String target = s;
+                for (int j = 0, k = 0; j < wartosciArg.length; j++) {
+                try {
+                    wartosciArg[j] = slowaPolecenia[i + j + 1];
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    if (strArr.length - 1 < k)
+                        wartosciArg[j] = "{arg}";
+                    else {
+                        wartosciArg[j] = strArr[k];
+                        newStrArr = new String[strArr.length - 1];
+                        System.arraycopy(strArr, k + 1, newStrArr, 0, newStrArr.length);
+                        strArr = newStrArr;
+                        System.out.println(Arrays.toString(strArr));
+                        k++;
                     }
                 }
+                target += " " + wartosciArg[j];
+                
+                }
+                i += wartosciArg.length - 1;
+                str = str.replace(target, parsuj(lambdaParsuj(hmPolecenia.get(s), wartosciArg), strArr));
+                } else // tu oprocz lambda abstrakcji mamy rowniez "normalne" argumenty funkcji jesli wystepuja overloady moga nastapic dodatkowe komplikacje
+                {
+                
+                Map<String, String> funkcje = hmPolecenia.entrySet().stream()
+                    .filter(entry -> entry.getKey().startsWith(s))
+                    .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+                
+                for (Map.Entry<String, String> entry : funkcje.entrySet()) {
+                
+                String[] deklaracjaFunkcji = entry.getKey().split(" ");
+                
+                boolean jestLiczba = true;
+                int iloscArgumentow = deklaracjaFunkcji.length;
+                String value = entry.getValue();
+                
+                String[] wartosciArg = new String[lambdaIleArg(value)];
+                
+                String target = deklaracjaFunkcji[0];
+                for (int j = 1; j < iloscArgumentow; j++) {
+                    jestLiczba = jestLiczba(deklaracjaFunkcji[j]);
+                    if (!jestLiczba) {
+                        value = zamienArgumenty(value, deklaracjaFunkcji[j], slowaPolecenia[j]);
+                        target += " " + slowaPolecenia[j];
+                    }
+                }
+                
+                for (int j = 0, k = 0; j < wartosciArg.length; j++) {
+                    try {
+                        wartosciArg[j] = slowaPolecenia[i + j + iloscArgumentow];
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        if (/*strArr.length - 1 < ktrue)
+                            wartosciArg[j] = "{arg}";
+                        else {
+                            wartosciArg[j] = strArr[k];
+                            newStrArr = new String[strArr.length - 1];
+                            System.arraycopy(strArr, k + 1, newStrArr, 0, newStrArr.length);
+                            strArr = newStrArr;
+                            System.out.println(Arrays.toString(strArr));
+                            k++;
+                        }
+                    }
+                }
+                i += iloscArgumentow - 1;
+                if (!jestLiczba)
+                    str = str.replace(target, parsuj(lambdaParsuj(value, wartosciArg), strArr));
+                }
+                }
+                
+                */
+
             } else if (hmPolecenia.get(s) != null) {
-                str = str.replace(s, parsuj(hmPolecenia.get(s), strArr));
+                str = zamienArgumenty(str, s, parsuj(hmPolecenia.get(s)));
 
             } else if (hmPolecenia.entrySet().stream().filter(entry -> entry.getKey().startsWith(s)).count() > 0) {
                 Map<String, String> funkcje = hmPolecenia.entrySet().stream()
@@ -469,10 +588,32 @@ public class Haskell {
             }
         }
 
+        if (slowaPolecenia(str).length > 1 && lambdaHoldery(str).size() > 0)
+            str = lambdaParsuj(str, new TreeMap<>(), new TreeMap<>());
+
+        if (slowaPolecenia(str).length == 1 && lambdaHoldery(str).size() == 0 && indeksyNawiasowKwad(str)[0] > -1)
+            str = parsuj(str.replace("[", "").replace("]", ""));
+
         if (jestLiczba(str) && Float.parseFloat(str) % 1 == 0) {
             str = str.replace(".0", "");
         }
+
         return str;
     }
 
+}
+
+class PolecenieLambda {
+    public String deklaracja;
+    public String polecenie;
+    public HashMap<String, String> args = new HashMap<String, String>();
+    public HashMap<String, String> lambdaArgs = new HashMap<String, String>();
+
+    PolecenieLambda(String deklaracja, String polecenie, HashMap<String, String> args,
+            HashMap<String, String> lambdaArgs) {
+        this.deklaracja = deklaracja;
+        this.polecenie = polecenie;
+        this.args = args;
+        this.lambdaArgs = lambdaArgs;
+    }
 }
